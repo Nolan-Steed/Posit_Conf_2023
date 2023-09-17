142da7c5a601bf07a76040d7757d458a2a896429
Nolan Steed <nolan.steed@roche.com>
2023-09-17 12:07:19 +0000

    Updated libraries and renv

---
+++ Intro_tidymodels/libraries.R
@@ -0,0 +1,27 @@
+# Install the packages for the workshop
+pkgs <- 
+  c("bonsai", "doParallel", "embed", "finetune", "lightgbm", "lme4",
+    "plumber", "probably", "ranger", "rpart", "rpart.plot", "rules",
+    "splines2", "stacks", "text2vec", "textrecipes", "tidymodels", 
+    "vetiver", "remotes")
+
+library('bonsai')
+library('doParallel')
+library('embed')
+library('finetune')
+library('lightgbm')
+library('lme4')
+library('plumber')
+library('probably')
+library('ranger')
+library('rpart')
+library('rpart.plot')
+library('rules')
+library('splines2')
+library('stacks')
+library('text2vec')
+library('textrecipes')
+library('tidymodels')
+library('vetiver')
+library('remotes')
+
